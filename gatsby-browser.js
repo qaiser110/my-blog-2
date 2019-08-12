60ed4bab16062ed8d4d4f682e4c7cda743f4f521
Qaiser Abbas <Abbas.Qaiser@abc.net.au>
2019-08-13 08:59:44 +1000

    layout that won't unmount with page transition

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+import React from 'react'
 import "./src/styles/global.css"
+import Layout from './src/components/layout'
+
+export const wrapPageElement = ({ element, props }) => {
+  // props provide same data to Layout as Page element will get
+  // including location, data, etc - you don't need to pass it
+  return <Layout {...props}>{element}</Layout>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -17,7 +16,7 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link>
     <br/>
     <Link to="/css-page/">Go to CSS page</Link>
-  </Layout>
+  </>
 )
 
 export default IndexPage
